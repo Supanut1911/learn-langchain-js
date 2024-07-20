cd6c32f7830c372db935b75ccf9abbb0c6cae18c
Supanut1911 <ynotmela@gmail.com>
2024-07-21 01:54:40 +0700

    prompt template, chain call chain

---
+++ prompt-template.js
@@ -0,0 +1,27 @@
+import { ChatOpenAI } from "@langchain/openai"
+import * as dotenv from "dotenv";
+import {ChatPromptTemplate} from '@langchain/core/prompts'
+
+dotenv.config();
+//create model
+const model = new ChatOpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    model: "gpt-3.5-turbo",
+    temperature: 0.7,   
+})
+
+//create prompt template
+const prompt = ChatPromptTemplate.fromMessages([
+    ["system", 'Generate a joke base on a word provided by the user'],
+    ["human", "{input}"]
+])
+
+// Create chain
+const chain = prompt.pipe(model)
+
+//call chain
+const response =  await chain.invoke({
+    input: "dog"
+})
+
+console.log(response);
